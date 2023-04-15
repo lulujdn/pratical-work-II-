7f490bd5aceb44262f3547bb588094e1bb3e94d4
Lulu <114011681+lulujdn@users.noreply.github.com>
2023-04-15 19:06:48 +0200

    Add files via upload

---
+++ MapModel.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Drawing;
+
+namespace BattleShip
+
+{
+    public class MapModel
+
+    {
+
+        public static void Main()
+
+
+        {
+            //  -------------------   CREATING THE MAPMODEL FOR THE GAME   ------------------------
+
+
+            Console.WriteLine("  - - - - -    B A T T L E S H I P  M A P !    - - - - - -  \n\n");
+
+
+            /*  > We start by creating an array in which we put the size of our matrix, being 12x12.
+                  The numbers go from 1 to 12 as seen in the foor loop.                          */
+
+
+            int[,] mapmodel = new int[12, 12];
+
+            for (int i = 1; i < 12; i++)
+
+            {
+                for (int j = 1; j < 12; j++)
+
+                {
+                    mapmodel[i, j] = i * j;
+                }
+
+            }
+
+
+
+            /*  > We use a bidimentional array in order to define two variables. In this case, we 
+                  have the "i" and the "j" that will store the vakues of the rows and columns of
+                  the matrix.                                                                  */
+
+
+
+            for (int i = 1; i < 12; i++)
+
+            {
+
+                Console.WriteLine("___\n");  // > Making spaces between each column.
+
+                for (int j = 1; j < 12; j++)
+
+                {
+
+                    if (i == 1 || j == 1)    /* > Creating a condition so that on only the first column
+                                                  an the firt row are visible in the map. So, if i and j 
+                                                  are equal to 1, the first row and column will show up. */
+
+                    {
+
+                        Console.Write("" + mapmodel[i, j] + " |  ");  // printing the first column and row. 
+
+                    }
+
+                    else    /* > Now, the rest of the numbers in the matrix wont be visible. They'll be 
+                                 substitued with the color blue to simulate the water.               */
+
+
+                    {
+
+                        Console.Write("   . "); // Mirarlo luego. 
+
+
+                    }
+
+                }
+
+                Console.WriteLine();
+
+            }
+
+            Console.ReadLine();
+
+        }
+
+    }
+
+}
+
---
+++ Menu.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BattleShip
+
+{
+    public class Program
+
+    {
+      
+        public static void Menu()
+
+        {
+            int option;
+        
+            while(true)
+
+
+            {
+
+              // ---------------------------------- C R E A T I N G  T H E  M E N U -------------------------------
+   
+                Console.WriteLine("    ___| |_____________________________________________________| |___      ");
+                Console.WriteLine("  ( ___   _____________________________________________________   ___ )    ");
+                Console.WriteLine("       | |                                                     | |         ");
+                Console.WriteLine("       | |                                                     | |         "); 
+                Console.WriteLine("       | |               WELCOME TO BATTLESHIP!                | |         ");
+                Console.WriteLine("       | |                                                     | |         ");
+                Console.WriteLine("       | |    Please choose an option down below:              | |         ");
+                Console.WriteLine("       | |                                                     | |         ");
+                Console.WriteLine("       | |    1. Load a previous game.                         | |         ");
+                Console.WriteLine("       | |                                                     | |         ");
+                Console.WriteLine("       | |    2. Two-Player game.                              | |         ");
+                Console.WriteLine("       | |                                                     | |         ");
+                Console.WriteLine("       | |    3. One-Player game.                              | |         ");
+                Console.WriteLine("       | |                                                     | |         ");
+                Console.WriteLine("       | |    4. Ranking.                                      | |         ");
+                Console.WriteLine("       | |                                                     | |         ");
+                Console.WriteLine("       | |    5. Exit game.                                    | |         ");
+                Console.WriteLine("       | |                                                     | |         ");
+                Console.WriteLine("    ___| |_____________________________________________________| |___      ");
+                Console.WriteLine("  ( ___   _____________________________________________________   ___ )    ");
+                Console.WriteLine("       | |                                                     | |         ");
+
+
+                option = Int32.Parse(Console.ReadLine());
+
+
+                switch (option)
+
+                {
+                    case 1:
+                        {
+                            Console.WriteLine("Which game would you like to load?");
+
+                            switch(option)
+
+                            {
+                                case 1:
+                                    break;
+                            }
+                            break;
+                        }
+
+                    case 2:
+                        {
+                            Console.WriteLine(" You've chosen two players.");
+                            break;
+                        }
+
+                    case 3:
+                        {
+                            Console.WriteLine(" You've chosen one player. ");
+                            break;
+                        }
+
+                    case 4:
+                        {
+                            Console.Write("Here's the current ranking. ");
+                            break;
+                        }
+
+                    case 5:
+                        {
+                            break;
+                        }
+
+
+                }
+
+
+            }
+           
+
+        }
+
+
+    }
+
+
+}
