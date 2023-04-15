708510040272b19e4933dea4fc943ed2ee28af7d
Lulu <114011681+lulujdn@users.noreply.github.com>
2023-04-15 21:59:19 +0200

    Update Ships.cs

--- Ships.cs
+++ Ships.cs
@@ -1,11 +1,40 @@
-﻿using System;
+using System;
+
+
 namespace BattleShip
+
 {
 	public class Ships
+
 	{
-		public Ships()
+
+		private int Xaxis;
+		private int Yaxis;
+
+
+		public Ships(int Xax, int Yax)
+
 		{
+			this.Xaxis = Xax;
+			this.Yaxis = Yax; 
+
 		}
-	}
+
+		public int GetSizeX()
+
+		{
+
+			return Xaxis;
+
+		}
+
+        public int GetSizeY()
+
+		{
+			return Yaxis;
+		}
+
+    }
+
 }
 
